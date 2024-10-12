c2de66515fff520da504bc29c42a345a3b13c8c6
Prasetyo Wicaksono <pras.wicak92@gmail.com>
2024-10-13 04:44:38 +0700

    feat: add cs fixer and phpstan

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,14 @@
+<?php
+
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__)
+    ->exclude('var')
+;
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@Symfony' => true,
+        '@PER-CS2.0' => true,
+    ])
+    ->setFinder($finder)
+;
