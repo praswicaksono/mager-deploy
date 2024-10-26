99d6698565c5f3eb8b2b5a3a8575eff2ac15784d
Prasetyo Wicaksono <pras.wicak92@gmail.com>
2024-10-26 21:25:17 +0700

    fix: phar wont run in other env

--- src/Kernel.php
+++ src/Kernel.php
@@ -8,4 +8,28 @@ use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    private string $projectDir;
+
+    public function getProjectDir(): string
+    {
+        if (!isset($this->projectDir)) {
+            $r = new \ReflectionObject($this);
+
+            if (!is_file($dir = $r->getFileName())) {
+                throw new \LogicException(sprintf('Cannot auto-detect project dir for kernel of class "%s".', $r->name));
+            }
+
+            $dir = $rootDir = \dirname($dir);
+            while (!is_file($dir . '/composer.json')) {
+                if ($dir === \dirname($dir)) {
+                    return \dirname(__DIR__);
+                }
+                $dir = \dirname($dir);
+            }
+            $this->projectDir = $dir;
+        }
+
+        return $this->projectDir;
+    }
 }
